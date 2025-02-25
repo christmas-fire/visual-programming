2f37aab07824d620d8408caaaadecc0625cf7b38
Бабешко Александр Владимирович <vpiskin335@gmail.com>
2025-02-25 10:13:35 +0700

    Мелкие правки

--- Homework-2/orderBy.test.js
+++ Homework-2/orderBy.test.js
@@ -2,32 +2,32 @@ const orderBy = require('./orderBy');
 
 test('Должен сортировать по имени и возрасту', () => {
     const data = [
-        { name: 'Alice', age: 30 },
-        { name: 'Bob', age: 25 },
-        { name: 'Alice', age: 20 }
+        { name: 'Александр', age: 19 },
+        { name: 'Настя', age: 8 },
+        { name: 'Лера', age: 19 }
     ];
     const sortedData = orderBy(data, ['name', 'age']);
     expect(sortedData).toEqual([
-        { name: 'Alice', age: 20 },
-        { name: 'Alice', age: 30 },
-        { name: 'Bob', age: 25 }
+        { name: 'Александр', age: 19 },
+        { name: 'Лера', age: 19 },
+        { name: 'Настя', age: 8 }
     ]);
 });
 
 test('Все элементы в массиве должны быть объектами', () => {
     const data = [
-        { name: 'Alice', age: 30 },
-        'Bob',
-        { name: 'Charlie', age: 25 }
+        { name: 'Наталья', age: 47 },
+        'Роман',
+        { name: 'Настя', age: 8 }
     ];
     expect(() => orderBy(data, ['name', 'age'])).toThrow('Все элементы в массиве должны быть объектами');
 });
 
 test('Во всех элементах присутствуют свойства, по которым выполняется сортировка', () => {
     const data = [
-        { name: 'Alice', age: 30 },
-        { name: 'Bob' },
-        { name: 'Charlie', age: 25 }
+        { name: 'Александр', age: 19 },
+        { name: 'Роман' },
+        { name: 'Михаил', age: 30 }
     ];
     expect(() => orderBy(data, ['name', 'age'])).toThrow('Свойство "age" отсутствует в одном из объектов');
 });
