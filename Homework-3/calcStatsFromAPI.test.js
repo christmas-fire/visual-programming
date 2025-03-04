2c613ffa5bca6bbaf4d2abbe0718c4f9d2bacd8d
Бабешко Александр Владимирович <vpiskin335@gmail.com>
2025-03-04 10:54:44 +0700

    Переделал и использованием spyOn

--- Homework-3/calcStatsFromAPI.test.js
+++ Homework-3/calcStatsFromAPI.test.js
@@ -1,30 +1,36 @@
 const calcStatsFromAPI = require('./calcStatsFromAPI');
 const loadData = require('./loadData');
 
-jest.mock('./loadData', () => jest.fn().mockResolvedValue([
-    {
-        breed: 'Turkish Van',
-        country: 'developed in the United Kingdom (founding stock from Turkey)',
-        origin: 'Natural',
-        coat: 'Semi-long',
-        pattern: 'Van'
-    },
-    {
-        breed: 'York Chocolate',
-        country: 'United States (New York)',
-        origin: 'Natural',
-        coat: 'Long',
-        pattern: 'Solid'
-    }
-]));
+test('Функция вызывается один раз', async () => {  
+    const result = jest.spyOn(loadData, "loadData");
+    result.mockImplementation(() => 'mock');
+    expect(loadData.loadData()).toEqual('mock')
 
-test('Загружает данные и возвращает корректный результат | вызывается 1 раз', async () => {
-    const result = await calcStatsFromAPI();
+    expect(result).toHaveBeenCalledTimes(1);
+});
 
-    expect(loadData).toHaveBeenCalledTimes(1);
+test('Результат соответствует ожидаемому', async () => {
+    const spy = jest.spyOn(loadData, 'loadData');
+    spy.mockResolvedValue([
+        {
+            breed: 'Turkish Van',
+            country: 'developed in the United Kingdom (founding stock from Turkey)',
+            origin: 'Natural',
+            coat: 'Semi-long',
+            pattern: 'Van',
+        },
+        {
+            breed: 'York Chocolate',
+            country: 'United States (New York)',
+            origin: 'Natural',
+            coat: 'Long',
+            pattern: 'Solid',
+        },
+    ]);
 
+    const result = await calcStatsFromAPI.calcStatsFromAPI();
     expect(result).toEqual({
         'developed in the United Kingdom (founding stock from Turkey)': 1,
-        'United States (New York)': 1
+        'United States (New York)': 1,
     });
 });
