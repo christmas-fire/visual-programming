ef575f05ba50b8c9d5c5a6470274a53e544552a8
Бабешко Александр Владимирович <vpiskin335@gmail.com>
2025-04-02 13:36:02 +0700

    Сдал работу

---
+++ Self-7/self-7/src/Accordion.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const Accordion = ({ header, children, renderHeader }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const renderedHeader = renderHeader(header, toggleAccordion);
+
+  return (
+    <div className="accordion">
+      <div className="header" onClick={toggleAccordion}>
+        {renderedHeader}
+      </div>
+
+      {isOpen && (
+        <div className={`body ${isOpen ? 'open' : ''}`}>
+          {children}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Accordion;
